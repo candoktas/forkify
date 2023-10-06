02a88ed19e7624e103483608ea29643f6fd6e8cf
candoktas <huseyincandoktas@gmail.com>
2023-10-06 14:45:23 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '26f8bc9a-3922-4fd4-b8ac-c18dbdac277e';
+export const MODAL_CLOSE_SEC = 2.5;
